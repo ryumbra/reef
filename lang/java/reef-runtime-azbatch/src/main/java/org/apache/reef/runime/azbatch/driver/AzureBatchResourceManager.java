0646d186bcaad1f9b84a99fee1015faa97b2a981
sharathmalladi <msharath@live.com>
2018-02-12 11:18:20 -0800

    Poll for Azure batch task status and report back to REEF (#24)
    Poll for Azure batch task status and report back to REEF

--- lang/java/reef-runtime-azbatch/src/main/java/org/apache/reef/runime/azbatch/driver/AzureBatchResourceManager.java
+++ lang/java/reef-runtime-azbatch/src/main/java/org/apache/reef/runime/azbatch/driver/AzureBatchResourceManager.java
@@ -29,9 +29,9 @@ import org.apache.reef.tang.annotations.Parameter;
 import org.apache.reef.tang.formats.ConfigurationSerializer;
 
 import javax.inject.Inject;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -45,24 +45,30 @@ public final class AzureBatchResourceManager {
   private static final Logger LOG = Logger.getLogger(AzureBatchResourceManager.class.getName());
 
   private final Map<String, ResourceRequestEvent> containerRequests;
-
+  private final Set<String> activeContainerIds;
   private final ConfigurationSerializer configurationSerializer;
   private final AzureBatchEvaluatorShimManager evaluatorShimManager;
+  private final AzureBatchTaskStatusAlarmHandler azureBatchTaskStatusAlarmHandler;
 
   private final double jvmHeapFactor;
   private final CommandBuilder launchCommandBuilder;
+  private final AtomicInteger containerCount;
 
   @Inject
   AzureBatchResourceManager(
       final ConfigurationSerializer configurationSerializer,
       final CommandBuilder launchCommandBuilder,
       final AzureBatchEvaluatorShimManager evaluatorShimManager,
+      final AzureBatchTaskStatusAlarmHandler azureBatchTaskStatusAlarmHandler,
       @Parameter(JVMHeapSlack.class) final double jvmHeapSlack) {
     this.configurationSerializer = configurationSerializer;
     this.evaluatorShimManager = evaluatorShimManager;
     this.jvmHeapFactor = 1.0 - jvmHeapSlack;
     this.launchCommandBuilder = launchCommandBuilder;
     this.containerRequests = new ConcurrentHashMap<>();
+    this.activeContainerIds = Collections.synchronizedSet(new HashSet<String>());
+    this.containerCount = new AtomicInteger(0);
+    this.azureBatchTaskStatusAlarmHandler = azureBatchTaskStatusAlarmHandler;
   }
 
   public void onResourceRequested(final ResourceRequestEvent resourceRequestEvent) {
@@ -70,27 +76,50 @@ public final class AzureBatchResourceManager {
     for (int r = 0; r < resourceRequestEvent.getResourceCount(); r++) {
       final String containerId = generateContainerId();
       this.containerRequests.put(containerId, resourceRequestEvent);
+      this.containerCount.incrementAndGet();
+      this.azureBatchTaskStatusAlarmHandler.enableAlarm();
       this.evaluatorShimManager.onResourceRequested(containerId, resourceRequestEvent);
     }
   }
 
   public void onResourceReleased(final ResourceReleaseEvent resourceReleaseEvent) {
-    LOG.log(Level.FINEST, "Got ResourceReleasedEvent for Id: {0} in AzureBatchResourceManager",
-        resourceReleaseEvent.getIdentifier());
-    this.containerRequests.remove(resourceReleaseEvent.getIdentifier());
+    String id = resourceReleaseEvent.getIdentifier();
+    LOG.log(Level.FINEST, "Got ResourceReleasedEvent for Id: {0} in AzureBatchResourceManager", id);
+    if (!this.activeContainerIds.remove(id)) {
+      LOG.log(Level.WARNING,
+          "Attempting to remove non-existent activeContainer for Id: {0} in AzureBatchResourceManager", id);
+    }
+
+    ResourceRequestEvent removedEvent = this.containerRequests.remove(id);
+    if (removedEvent == null) {
+      LOG.log(Level.WARNING,
+          "Ignoring attempt to remove non-existent containerRequest for Id: {0} in AzureBatchResourceManager", id);
+    } else {
+      int currentContainerCount = this.containerCount.decrementAndGet();
+      if (currentContainerCount <= 0) {
+        this.azureBatchTaskStatusAlarmHandler.disableAlarm();
+      }
+    }
+
     this.evaluatorShimManager.onResourceReleased(resourceReleaseEvent);
   }
 
   public void onResourceLaunched(final ResourceLaunchEvent resourceLaunchEvent) {
-    final int evaluatorMemory = this.containerRequests.get(resourceLaunchEvent.getIdentifier()).getMemorySize().get();
+    String id = resourceLaunchEvent.getIdentifier();
+    LOG.log(Level.FINEST, "Got ResourceLaunchEvent for Id: {0} in AzureBatchResourceManager", id);
+    final int evaluatorMemory = this.containerRequests.get(id).getMemorySize().get();
     String launchCommand = this.launchCommandBuilder.buildEvaluatorCommand(resourceLaunchEvent,
         evaluatorMemory, this.jvmHeapFactor);
     String evaluatorConfigurationString = this.configurationSerializer.toString(resourceLaunchEvent.getEvaluatorConf());
-
+    this.activeContainerIds.add(id);
     // TODO[At this point we are only sending the evaluator config and launch command to the shim. AllocatedEvaluator]
     // TODO[object also has "addFile" and "addLibrary" methods, so maybe we'll need to package the JAR for the]
     // TODO[evaluator, too, and not assume that both Evaluator and Shim can share the same JAR.]
     this.evaluatorShimManager.onResourceLaunched(resourceLaunchEvent, launchCommand, evaluatorConfigurationString);
+  }
+
+  public Boolean isContainerActive(final String containerId) {
+    return this.activeContainerIds.contains(containerId);
   }
 
   private String generateContainerId() {
--- lang/java/reef-runtime-azbatch/src/main/java/org/apache/reef/runime/azbatch/util/AzureBatchHelper.java
+++ lang/java/reef-runtime-azbatch/src/main/java/org/apache/reef/runime/azbatch/util/AzureBatchHelper.java
@@ -16,55 +16,66 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.reef.runime.azbatch.client;
+package org.apache.reef.runime.azbatch.util;
 
 import com.microsoft.azure.batch.BatchClient;
 import com.microsoft.azure.batch.auth.BatchSharedKeyCredentials;
 import com.microsoft.azure.batch.protocol.models.*;
-import org.apache.reef.runime.azbatch.util.AzureBatchFileNames;
 
+import org.apache.reef.runime.azbatch.client.AzureBatchJobSubmissionHandler;
+import org.apache.reef.runime.azbatch.parameters.AzureBatchAccountKey;
+import org.apache.reef.runime.azbatch.parameters.AzureBatchAccountName;
+import org.apache.reef.runime.azbatch.parameters.AzureBatchAccountUri;
+import org.apache.reef.runime.azbatch.parameters.AzureBatchPoolId;
+import org.apache.reef.tang.annotations.Parameter;
+
+import javax.inject.Inject;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * A {@link AzureBatchJobSubmissionHelper} for Azure Batch.
+ * A {@link AzureBatchHelper} for Azure Batch.
  */
-public class AzureBatchJobSubmissionHelper implements AutoCloseable {
+public class AzureBatchHelper {
 
   private static final Logger LOG = Logger.getLogger(AzureBatchJobSubmissionHandler.class.getName());
+
+  /*
+   * Environment variable that contains the Azure Batch jobId.
+   */
+  private static final String AZ_BATCH_JOB_ID_ENV = "AZ_BATCH_JOB_ID";
 
   private final String azureBatchAccountUri;
   private final String azureBatchAccountName;
   private final String azureBatchAccountKey;
   private final String azureBatchPoolId;
+  private final BatchClient client;
+  private final PoolInformation poolInfo;
 
-  private final String applicationId;
-
-  public AzureBatchJobSubmissionHelper(
-      final String azureBatchAccountUri,
-      final String azureBatchAccountName,
-      final String azureBatchAccountKey,
-      final String azureBatchPoolId,
-      final String applicationId) {
+  @Inject
+  public AzureBatchHelper(
+      @Parameter(AzureBatchAccountUri.class) final String azureBatchAccountUri,
+      @Parameter(AzureBatchAccountName.class) final String azureBatchAccountName,
+      @Parameter(AzureBatchAccountKey.class) final String azureBatchAccountKey,
+      @Parameter(AzureBatchPoolId.class) final String azureBatchPoolId) {
     this.azureBatchAccountUri = azureBatchAccountUri;
     this.azureBatchAccountName = azureBatchAccountName;
     this.azureBatchAccountKey = azureBatchAccountKey;
     this.azureBatchPoolId = azureBatchPoolId;
-    this.applicationId = applicationId;
-  }
-
-  public void submit(final URI jobJarSasUri, final String command) throws BatchErrorException, IOException {
 
     BatchSharedKeyCredentials cred = new BatchSharedKeyCredentials(
         this.azureBatchAccountUri, this.azureBatchAccountName, this.azureBatchAccountKey);
-    BatchClient client = BatchClient.open(cred);
+    this.client = BatchClient.open(cred);
 
-    PoolInformation poolInfo = new PoolInformation();
+    this.poolInfo = new PoolInformation();
     poolInfo.withPoolId(this.azureBatchPoolId);
+  }
 
+  public void submitJob(final String applicationId, final URI jobJarSasUri, final String command) throws IOException {
     ResourceFile jarResourceFile = new ResourceFile()
         .withBlobSource(jobJarSasUri.toString())
         .withFilePath(AzureBatchFileNames.TASK_JAR_FILE_NAME);
@@ -85,8 +96,20 @@ public class AzureBatchJobSubmissionHelper implements AutoCloseable {
     client.jobOperations().createJob(jobAddParameter);
   }
 
-  @Override
-  public void close() {
+  public List<CloudTask> getTaskStatusForJob(final String jobId) {
+    List<CloudTask> tasks = null;
+    try {
+      tasks = client.taskOperations().listTasks(jobId);
+      LOG.log(Level.INFO, "Task status for job: {0} returned {1} tasks", new Object[]{jobId, tasks.size()});
+    } catch (IOException | BatchErrorException ex) {
+      LOG.log(Level.SEVERE, "Exception when fetching Task status for job: {0}. Exception [{1}]:[2]",
+          new Object[] {jobId, ex.getMessage(), ex.getStackTrace()});
+    }
 
+    return tasks;
+  }
+
+  public String getAzureBatchJobId() {
+    return System.getenv(AZ_BATCH_JOB_ID_ENV);
   }
 }
