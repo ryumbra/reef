36571b8dbb8f331bf82dcfe434ecb5986fc24c23
Reed Umbrasas <ryumbra@microsoft.com>
2018-02-27 10:58:15 -0800

    Adding JobJarMaker

--- lang/cs/Org.Apache.REEF.Client/AzureBatch/AzureBatchDotNetClient.cs
+++ lang/cs/Org.Apache.REEF.Client/AzureBatch/AzureBatchDotNetClient.cs
@@ -1,4 +1,21 @@
-﻿using Org.Apache.REEF.Client.API;
+﻿// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using Org.Apache.REEF.Client.API;
 using Org.Apache.REEF.Client.Common;
 using Org.Apache.REEF.Client.YARN.RestClient.DataModel;
 using System;
@@ -17,7 +34,7 @@ namespace Org.Apache.REEF.Client.DotNet.AzureBatch
     {
         private static readonly Logger Log = Logger.GetLogger(typeof(AzureBatchDotNetClient));
         private readonly IInjector _injector;
-        private readonly IResourceArchiveFileGenerator _resourceArchiveFileGenerator;
+        
         private readonly DriverFolderPreparationHelper _driverFolderPreparationHelper;
         private readonly REEFFileNames _fileNames;
         private readonly IStorageUploader _storageUploader;
@@ -36,7 +53,6 @@ namespace Org.Apache.REEF.Client.DotNet.AzureBatch
             AzureBatchService batchService)
         {
             _injector = injector;
-            _resourceArchiveFileGenerator = resourceArchiveFileGenerator;
             _fileNames = fileNames;
             _driverFolderPreparationHelper = driverFolderPreparationHelper;
             _storageUploader = storageUploader;
@@ -59,7 +75,6 @@ namespace Org.Apache.REEF.Client.DotNet.AzureBatch
             var configModule = AzureBatchClientConfiguration.ConfigurationModule;
             string jobId = jobRequest.JobIdentifier;
             string commandLine = GetCommand(jobRequest.JobParameters);
-
             _batchService.CreateJob(jobId, null, commandLine);
         }
 
