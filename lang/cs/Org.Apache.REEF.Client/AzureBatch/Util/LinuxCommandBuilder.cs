cd8c811c7d38a4273cacb992a18e5f2d42c8ff8c
Chenxi Zhao <33109414+chzhams@users.noreply.github.com>
2018-03-13 13:55:10 -0700

    Minor typo fix

--- lang/cs/Org.Apache.REEF.Client/AzureBatch/Util/LinuxCommandBuilder.cs
+++ lang/cs/Org.Apache.REEF.Client/AzureBatch/Util/LinuxCommandBuilder.cs
@@ -24,7 +24,7 @@ namespace Org.Apache.REEF.Client.AzureBatch.Util
     internal sealed class LinuxCommandBuilder : AbstractCommandBuilder
     {
         private static readonly string CommandPrefix =
-            "unzip " + AzureBatchFileNames.GetTaskJarFileName() + " d 'reef/'" + ";";
+            "unzip " + AzureBatchFileNames.GetTaskJarFileName() + " -d 'reef/'" + ";";
         private const string ClassPathSeparator = ":";
         private const string OsCommandFormat = "/bin/sh c \"{0}\"";
 
